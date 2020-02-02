9f447d2cc5d9fcb9696dff7e785aa2c3180515f6
Luka Dimnik <luka.dimnik88@gmail.com>
2020-02-02 15:59:49 +0200

    Updates

---
+++ precache-manifest.5c8cea2d290d93b48194c3d92ec7af19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa1669c78026e68a03f78cf33f02972",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "044c8e470317566e3477",
+    "url": "/portfolio/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "9c58c6da3d649efd2a8e",
+    "url": "/portfolio/static/css/main.aa311818.chunk.css"
+  },
+  {
+    "revision": "044c8e470317566e3477",
+    "url": "/portfolio/static/js/2.5975bd64.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/portfolio/static/js/2.5975bd64.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c58c6da3d649efd2a8e",
+    "url": "/portfolio/static/js/main.dd06fe7e.chunk.js"
+  },
+  {
+    "revision": "06a05004aa828f089725",
+    "url": "/portfolio/static/js/runtime-main.083e83d4.js"
+  }
+]);
